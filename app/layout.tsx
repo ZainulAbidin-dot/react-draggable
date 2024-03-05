25f8974d5a7b2b6467e62e3f3d7a67af71a36246
ZainulAbidin-dot <zchxvr@gmail.com>
2024-03-05 19:19:56 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PageForm (@CodeWithKliton)",
-  description: "Check my youtube channel for more content",
+  title: "Draggable form builder",
+  description: "Draggable form builder",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- components/CreateFormBtn.tsx
+++ components/CreateFormBtn.tsx
@@ -53,7 +53,7 @@ function CreateFormBtn() {
           className="group border border-primary/20 h-[190px] items-center justify-center flex flex-col hover:border-primary hover:cursor-pointer border-dashed gap-4"
         >
           <BsFileEarmarkPlus className="h-8 w-8 text-muted-foreground group-hover:text-primary" />
-          <p className="font-bold text-xl text-muted-foreground group-hover:text-primary">Create new form</p>
+          <p className="font-bold text-xl text-muted-foreground group-hover:text-primary">New form</p>
         </Button>
       </DialogTrigger>
       <DialogContent>
